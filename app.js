0596838889ed06035bdbb27ca0c332e5f633e102
Tobias Baunbaek <freeall@gmail.com>
2023-11-21 16:45:52 +0100

    Share file into own hyperdrive, and download from others

--- app.js
+++ app.js
@@ -3,133 +3,170 @@ import Hyperswarm from 'hyperswarm'
 import Hyperdrive from 'hyperdrive'
 import Corestore from 'corestore'
 import ProtomuxRPC from 'protomux-rpc'
+import Localdrive from 'localdrive'
+import path from 'path'
 
 const { app } = await versions()
 const name = pear.config.storage.split('/').pop() // Since we can't pass args to the app for now, this is a hack to do that
 const store = new Corestore(pear.config.storage)
 const drive = new Hyperdrive(store)
+const localDrive = new Localdrive('./files') // Would prefer to have a "save as.." filer
 const peers = {}
 const $peers = document.querySelector('#peers')
 const $files = document.querySelector('#files')
-const $file = document.querySelector('#file')
+const $uploadFile = document.querySelector('#upload-file')
+const $createFile = document.querySelector('#create-file')
 const $name = document.querySelector('#name')
 const swarm = new Hyperswarm({
   keyPair: await store.createKeyPair('first-app')
 })
-let sharedFilename
 
-initFileInput()
+teardown(async () => {
+  await swarm.destroy()
+  await store.close()
+})
+
+initFileInputs()
 await initSwarm()
 await drive.ready()
-drive.findingPeers()
-startWatcher()
+startWatcher(drive)
 await render()
-// await drive.put('/hello_world.txt', Buffer.from('hello world'))
 console.log('Startup completed')
 
-
-async function startWatcher() {
+async function startWatcher(drive) {
   const watcher = drive.watch('/')
-  for await (const [current, previous] of watcher) {
-    console.log(current.version)
-    console.log(previous.version)
+  for await (const _ of watcher) {
+    render()
   }
 }
 
-function initFileInput() {
-  $file.addEventListener('change', e => {
-    console.log(e.target.files)
-    const hasFiles = e.target.files.length > 0
-    if (!hasFiles) return
-    sharedFilename = e.target.files[0].name
-    Object.values(peers).forEach(conn => conn.write(JSON.stringify({ type: 'file', filename: sharedFilename })))
+function initFileInputs() {
+  $createFile.addEventListener('click', async e => {
+    const filename = Math.floor(1000000000 * Math.random())
+    await drive.put(`/${filename}.txt`, Buffer.from('hello world'))
+    console.log('Random file added')
+  })
+
+  $uploadFile.addEventListener('change', async e => {
+    for (const file of e.target.files) {
+      const data = await file.arrayBuffer()
+      drive.put(`/${file.name}`, data)
+    }
   })
 }
 
 async function initSwarm() {
-  teardown(() => swarm.destroy())
-
   swarm.on('connection', (conn, info) => {
     console.log('[connection joined]', info)
 
-    const key = info.publicKey.join(',')
+    const key = info.publicKey.toString('hex')
     peers[key] = conn
 
     conn.setKeepAlive(5000)
 
     store.replicate(conn)
 
-    if (sharedFilename) {
-      conn.write(JSOIN.stringify({ type: 'file', filename: sharedFilename }))
-    }
-
     const rpc = new ProtomuxRPC(conn)
     // Register for when the peer says hello to us
-    rpc.respond('hello', req => {
+    rpc.respond('hello', async req => {
       const { name, driveKey } = JSON.parse(req.toString())
+      console.log(`[hello] name=${name} driveKey=${driveKey}`)
       conn.name = name
-      conn.drive = new Hyperdrive(store, driveKey)
-      console.log('received hello')
-      console.log(name, driveKey)
+      const drive = new Hyperdrive(store, driveKey)
+      await drive.ready()
+      const files = await ls({ drive, path: '/' })
+      conn.drive = drive
+      startWatcher(drive) // render, when a file is added/removed
       render()
     })
+
     // Say hello to the peer
     rpc.request('hello', Buffer.from(JSON.stringify({ name, driveKey: drive.key.toString('hex') })))
 
     conn.on('error', err => console.error(err)) // Needed because otherwise teardowns result in exceptions
-    conn.on('close', () => {
+    conn.on('close', async () => {
       delete peers[key]
-      console.log(`[connection left] ${conn.name || 'No name'}`) // (${key})`)
+      console.log(`[connection left] ${conn.name}`)
       render()
     })
 
     render()
   })
+
   const topic = Buffer.from(app.key || 'W3z8fsASq1O1Zm8oPEvwBYbN2Djsw97R')
   const discovery = swarm.join(topic, { server: true, client: true })
   await discovery.flushed()
 }
 
-async function ls(path) {
-  const stream = await drive.list(path, {
+async function ls({ drive, path }) {
+  const stream = drive.list(path, {
     recursive: false
   })
   const files = []
-  for await (const f of stream) {
-    files.push(f)
+  for await (const file of stream) {
+    files.push(file)
   }
+
   return files
 }
 
 async function render() {
-  const files = await ls('/')
-  const peersSockets = Object.entries(peers)
-  const hasPeers = peersSockets.length > 0
-  const hasFiles = files.length > 0
-  const peersElems = peersSockets.map(([id, conn]) => `
-    <div>
-      ${conn.name || 'No name'}
-      ${conn.filename
-      ? `(<a href="#">${conn.filename}</a>)`
-      : ''
-    }
-    </div>
-  `)
-  const filesElems = files.map(({ key }) => `
-    <div>
-      ${key}
-    </div>
-  `)
+  const peersConn = Object.values(peers)
+  const hasPeers = peersConn.length > 0
+  const filesElems = await generateFolderDom({ drive, allowDeletion: true })
+  const hasFiles = filesElems.length > 0
+
+  const peersElems = []
+  for (const conn of peersConn) {
+    const $wrapper = document.createElement('div')
+
+    const $name = document.createElement('div')
+    $name.innerText = conn.name
+
+    const $files = document.createElement('div')
+    $files.innerText = 'No files shared'
+    const peerFilesElems = await generateFolderDom({
+      drive: conn.drive,
+      allowDeletion: false
+    })
+    const hasFiles = peerFilesElems.length > 0
+    if (hasFiles) $files.replaceChildren(...peerFilesElems)
+
+    $wrapper.appendChild($name)
+    $wrapper.appendChild($files)
+    peersElems.push($wrapper)
+  }
 
   $name.innerText = `(${name})`
-  $peers.innerHTML = `
-    <div>
-      ${hasPeers ? peersElems.join('\n') : 'No peers connected'}
-    </div>
-  `
-  $files.innerHTML = `
-    <div>
-      ${hasFiles ? filesElems.join('\n') : 'No files in this folder'}
-    </div>
-  `
+  $peers.replaceChildren(...peersElems)
+  $files.replaceChildren(...filesElems)
+}
+
+async function generateFolderDom({ drive, allowDeletion = false }) {
+  if (!drive) return []
+  const files = await ls({ drive, path: '/' })
+  const filesElems = []
+  for (const file of files) {
+    const $wrapper = document.createElement('div')
+
+    const $file = document.createElement('span')
+    $file.className = 'file'
+    $file.innerText = file.key
+    $file.addEventListener('click', async () => {
+      const buffer = await drive.get(file.key)
+      await localDrive.put(file.key, buffer)
+    })
+
+    const $delete = document.createElement('span')
+    $delete.className = 'delete'
+    $delete.innerText = '❌'
+    $delete.addEventListener('click', () => drive.del(file.key))
+
+    $wrapper.appendChild($file)
+    if (allowDeletion) $wrapper.appendChild($delete)
+
+    filesElems.push($wrapper)
+  }
+
+  return filesElems
 }
